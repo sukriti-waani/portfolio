4063821a22f0f8b05789940660b90659af4e23b2
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-06-04 10:43:53 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "aos/dist/aos.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import { About } from "./components/About/About";
-import { Skills } from "./components/Skills/skills";
+import { Skills } from "./components/Skills/Skills";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
 
